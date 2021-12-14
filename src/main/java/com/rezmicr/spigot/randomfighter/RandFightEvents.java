9191375f086801b14d265d97eaa24f6b721d57ff
RezmiCR <luisross87@gmail.com>
2021-12-13 23:02:08 -0600

    fixes and added items with effects.
    - RandomTypes now work as intended
    - New items for the drop pool
    - New special effects when using some items
    - New bugs

--- src/main/java/com/rezmicr/spigot/randomfighter/RandFightEvents.java
+++ src/main/java/com/rezmicr/spigot/randomfighter/RandFightEvents.java
@@ -15,10 +15,13 @@ import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 
 import org.bukkit.block.Block;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Mob;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.entity.SlimeSplitEvent;
@@ -37,6 +40,7 @@ public class RandFightEvents implements Listener {
     private final List<Material> itemPool = new ArrayList<Material>();
     private final Random gen = new Random();
     private final RandomTypes randTypes;
+    private final Material[] chickenStuff = new Material[]{Material.STONE_PICKAXE,Material.SHIELD,Material.STONE_SWORD};
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
@@ -46,8 +50,14 @@ public class RandFightEvents implements Listener {
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
         Player player = event.getPlayer();
-        if (player.getInventory().getItemInMainHand().getType() == Material.FISHING_ROD) {
-            player.getWorld().strikeLightning(player.getTargetBlock((Set<Material>) null, 200).getLocation());
+        ItemStack itemInHand = player.getInventory().getItemInMainHand(); 
+        switch (itemInHand.getType()) {
+            case FISHING_ROD:
+                player.getWorld().strikeLightning(player.getTargetBlock((Set<Material>) null, 200).getLocation());
+                break;
+            case SPYGLASS:
+                player.teleport(player.getTargetBlock(null, 20).getLocation());
+                break;
         }
     }
 
@@ -85,41 +95,71 @@ public class RandFightEvents implements Listener {
                 this.plugin.getScoreBoard().updatePlayer(killer);
                 killer.sendMessage("1 point kill");
         }
+        } else {
+            List<Entity> near = entity.getNearbyEntities(10,10,10);
+            for (Entity e : near) {
+                if (e.getScoreboardTags().contains("random_player")) {
+                    killer.sendMessage("1 point shared kill");
+                    this.plugin.getScoreBoard().updatePlayer((Player) e);
+                }
+            }
         }
         EntityType type = entity.getType();
         List<ItemStack> items = event.getDrops();
         items.clear();
         // define special items to always drop for some specific creatures
-        switch (type) {
-            case CHICKEN:
-                items.add(new ItemStack(Material.GOLDEN_CARROT,6));
-                break;
+        if (type == EntityType.CHICKEN) {
+                items.add(new ItemStack(chickenStuff[gen.nextInt(2)],1));
+                items.add(new ItemStack(randTypes.randItem(),1));
         }
         // add a random item from a pre defined pool
-        items.add(new ItemStack(randTypes.randItem(),gen.nextInt(2)));
+        items.add(new ItemStack(randTypes.randItem(),1));
     }
 
     @EventHandler
     public void onEntityDamaged(EntityDamageByEntityEvent event) {
         if (event.getDamager() instanceof Player) {
         Player player = (Player) event.getDamager();
+        PotionEffect effect;
         // Verify if the player is currently in a game
         if (player.getScoreboardTags().contains("random_fighter")) {
             Location loc = (Location) player.getLocation();
             loc.setY(loc.getY() - 1);
             Block bloke = (Block) loc.getBlock();
             // Different effects based on the weapon used
-            if (player.getInventory().getItemInMainHand().getType() == Material.STONE_PICKAXE) {
-                bloke.setType(Material.STONE);
-            } else if (player.getInventory().getItemInMainHand().getType() == Material.BUCKET) {
-                bloke.setType(Material.WATER);
-            } else if (player.getInventory().getItemInMainHand().getType() == Material.FEATHER) {
-                PotionEffect effect = new PotionEffect(PotionEffectType.LEVITATION,10*20,1);
-                effect.apply((LivingEntity) event.getEntity());
+            Material itemInHand = player.getInventory().getItemInMainHand().getType();
+            switch (itemInHand) {
+                case STONE_PICKAXE:
+                    if (bloke.getType() != Material.BARRIER || bloke.getType() != Material.BEDROCK)
+                        bloke.setType(Material.STONE);
+                    break;
+                case BUCKET:
+                    if (bloke.getType() != Material.BARRIER || bloke.getType() != Material.BEDROCK)
+                        bloke.setType(Material.WATER);
+                    break;
+                case FEATHER:
+                    effect = new PotionEffect(PotionEffectType.LEVITATION,10*20,1);
+                    effect.apply((LivingEntity) event.getEntity());
+                    break;
+                case POISONOUS_POTATO:
+                    Mob damagee = ((Mob) event.getEntity());
+                    effect = new PotionEffect(PotionEffectType.POISON,10*20,1);
+                    damagee.setHealth(2);
+                    effect.apply(damagee);
+                    player.getInventory().getItemInMainHand().setType(Material.BAKED_POTATO);
+                    break;
+                case CLOCK:
+                    effect = new PotionEffect(PotionEffectType.SLOW,10*20,1);
+                    effect.apply((LivingEntity) event.getEntity());
+                    break;
+                case SEA_LANTERN:
+                    effect = new PotionEffect(PotionEffectType.GLOWING,10*20,1);
+                    effect.apply((LivingEntity) event.getEntity());
             }
         }
         }
     }
+
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent event) {
     Player player = event.getEntity();
@@ -128,6 +168,7 @@ public class RandFightEvents implements Listener {
         this.deathLocs.put(player,location);
     }
     }
+
     @EventHandler
     public void onPlayerRespawn(PlayerRespawnEvent event) {
     Player player = event.getPlayer();
--- src/main/java/com/rezmicr/spigot/randomfighter/RandomTypes.java
+++ src/main/java/com/rezmicr/spigot/randomfighter/RandomTypes.java
@@ -12,37 +12,31 @@ public class RandomTypes {
 
     private class EntityChance {
 
-        private final HashMap<Integer,EntityType> entities;
-        private final ArrayList<EntityType> entityChance = new ArrayList<EntityType>();
+        private final HashMap<EntityType, Integer> entities = new HashMap<>();
+        private final ArrayList<EntityType> entityChance = new ArrayList<>();
         private int totalWeigth;
         private final Random random; 
         private boolean built;
 
         public EntityChance(Random random) {
-            entities = new HashMap<Integer,EntityType>();
             this.random = random;
         }
 
         public void add(EntityType type,int weigth) {
-            entities.put(weigth,type);
+            entities.put(type, weigth);
             totalWeigth += weigth;
         }
 
         public EntityType getRandomEntity() {
-            if (entities.size() == 0)
-                return null;
-            if (!built)
-                build();
-            //System.err.println(totalWeigth);
-            //System.err.println(entityChance.size());
-            int x = random.nextInt(entityChance.size());
-            return entityChance.get(x);
+            if (entities.size() == 0) return null;
+            if (!built) build();
+            return entityChance.get(random.nextInt(totalWeigth));
         }
 
         public void build() {
-            for (Integer i : entities.keySet()) {
-                for (int x = 0; x <= i; x++) {
-                    entityChance.add(entities.get(i));
+            for (EntityType type : entities.keySet()) {
+                for (int x = 0; x < entities.get(type); x++) {
+                    entityChance.add(type);
                 }
             }
             built = true;
@@ -51,37 +45,31 @@ public class RandomTypes {
 
     private class ItemChance {
 
-        private final HashMap<Integer,Material> items;
-        private final ArrayList<Material> itemChance = new ArrayList<Material>();
+        private final HashMap<Material, Integer> items = new HashMap<>();
+        private final ArrayList<Material> itemChance = new ArrayList<>();
         private int totalWeigth;
         private final Random random; 
         private boolean built;
 
         public ItemChance(Random random) {
-            items = new HashMap<Integer,Material>();
             this.random = random;
         }
 
         public void add(Material material,int weigth) {
-            items.put(weigth,material);
+            items.put(material, weigth);
             totalWeigth += weigth;
         }
 
         public Material getRandomItem() {
-            if (items.size() == 0)
-                return null;
-            if (!built)
-                build();
-            //System.err.println(totalWeigth);
-            //System.err.println(itemChance.size());
-            int x = random.nextInt(itemChance.size());
-            return itemChance.get(x);
+            if (items.size() == 0) return null;
+            if (!built) build();
+            return itemChance.get(random.nextInt(totalWeigth));
         }
 
         public void build() {
-            for (Integer i : items.keySet()) {
-                for (int x = 0; x <= i; x++) {
-                    itemChance.add(items.get(i));
+            for (Material mat : items.keySet()) {
+                for (int x = 0; x < items.get(mat); x++) {
+                    itemChance.add(mat);
                 }
             }
             built = true;
@@ -93,45 +81,39 @@ public class RandomTypes {
     private final EntityChance entities;
     
     public RandomTypes() {
-        // weigth from 1 to 10
         random = new Random();
         items = new ItemChance(random);
         entities = new EntityChance(random);
 
         // hardcoded pools TODO: (for now)
-        entities.add(EntityType.CREEPER,5);
+        entities.add(EntityType.CREEPER,3);
         entities.add(EntityType.HUSK,5);
-        entities.add(EntityType.SLIME,2);
-        entities.add(EntityType.PIGLIN_BRUTE,4);
-        //entities.add(EntityType.CAVE_SPIDER,3);
+        entities.add(EntityType.SLIME,4);
+        entities.add(EntityType.WITCH,3);
+        entities.add(EntityType.VEX,1);
         entities.add(EntityType.VINDICATOR,1);
         entities.add(EntityType.ZOMBIE_HORSE,3);
-        entities.build();
 
-        items.add(Material.LEATHER_BOOTS,3);
-        items.add(Material.LEATHER_HELMET,3);
-        items.add(Material.LEATHER_CHESTPLATE,3);
-        items.add(Material.LEATHER_LEGGINGS,3);
-        items.add(Material.IRON_BOOTS,2);
-        items.add(Material.IRON_HELMET,2);
-        items.add(Material.IRON_CHESTPLATE,2);
-        items.add(Material.IRON_LEGGINGS,2);
+        items.add(Material.LEATHER_BOOTS,2);
+        items.add(Material.LEATHER_HELMET,2);
+        items.add(Material.LEATHER_CHESTPLATE,2);
+        items.add(Material.LEATHER_LEGGINGS,2);
         items.add(Material.DIAMOND_BOOTS,1);
         items.add(Material.DIAMOND_HELMET,1);
         items.add(Material.DIAMOND_CHESTPLATE,1);
         items.add(Material.DIAMOND_LEGGINGS,1);
-        items.add(Material.FISHING_ROD,4);
-        items.add(Material.STONE_PICKAXE,6);
-        items.add(Material.SPYGLASS,4);
-        items.add(Material.BUCKET,4);
-        items.add(Material.FEATHER,7);
-        items.add(Material.IRON_AXE,1);
-        items.add(Material.GOLDEN_CARROT,6);
-        items.add(Material.CARROT,6);
-        items.add(Material.POISONOUS_POTATO,4);
-        items.add(Material.CLOCK,3);
-        items.add(Material.SEA_LANTERN,3);
-        items.build();
+        items.add(Material.FISHING_ROD,2);
+        items.add(Material.STONE_PICKAXE,5);
+        //items.add(Material.SPYGLASS,4);
+        items.add(Material.DIAMOND_SWORD,1);
+        items.add(Material.BUCKET,3);
+        items.add(Material.FEATHER,3);
+        items.add(Material.IRON_AXE,4);
+        items.add(Material.GOLDEN_CARROT,4);
+        items.add(Material.CARROT,5);
+        items.add(Material.POISONOUS_POTATO,2);
+        items.add(Material.CLOCK,1);
+        items.add(Material.SEA_LANTERN,2);
     }
 
     public EntityType randEntity() {
